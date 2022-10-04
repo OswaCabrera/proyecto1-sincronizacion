b02f2cbf2878c12d4d43accd4a5ab3d4154b6061
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2022-10-04 13:40:51 -0500

    Creo cliente y servidor

---
+++ Algoritmo de Cristian/cliente.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from socket import *
+
+import time
+import datetime
+
+def convertir_cadena_a_hora(cadena):
+    formato = '%Y%m%d %H:%M:%f'
+
+    hora_cadena = datetime.datetime.strptime(cadena, formato)
+
+    return hora_cadena
+
+IPServidor = "localhost"
+puertoServidor = 8000
+socketCliente = socket(AF_INET, SOCK_STREAM)
+
+inicio = time.time()
+socketCliente.connect((IPServidor, puertoServidor))
+
+hora_cadena = socketCliente.recv(4096).decode()
+
+final = time.time()
+tiempo = final - inicio
+hora = convertir_cadena_a_hora(hora_cadena)
+
+print("El tiempo total de ida y vuelta fue:", tiempo)
+
+mitadTiempo = tiempo/2
+
+print("El tiempo de ida: ", mitadTiempo)
+
+print("Hora servidor: ", hora_cadena)
+print("La hora exacta es : ", hora + datetime.timedelta(seconds=mitadTiempo))
+
+socketCliente.close()
---
+++ Algoritmo de Cristian/servidor.py
@@ -0,0 +1,43 @@
+from socket import *
+from datetime import *
+    
+def prenderServer(): 
+  
+    s = socket( AF_INET, SOCK_STREAM) 
+    print("Socket creado exitosamente") 
+        
+    
+    port = 8000
+    direccionServidor = "localhost"
+  
+    s.bind((direccionServidor, port)) 
+       
+    
+    s.listen(5)       
+    print("Socket escuchando...") 
+        
+    
+    while True:  
+        
+       
+       connection, address = s.accept()       
+       print('Servidor conectado a', address)
+
+       print('Servidor conectado a', address)
+       
+       hora = datetime.now()
+
+       horaCadena = hora.strftime("%Y%m%d %H:%M:%f") 
+
+       print('Enviamos la hora al cliente', address)
+       
+       connection.send(horaCadena.encode())
+
+       connection.close() 
+       print('Terminamos la conexión') 
+  
+  
+if __name__ == '__main__': 
+  
+    
+    prenderServer()
